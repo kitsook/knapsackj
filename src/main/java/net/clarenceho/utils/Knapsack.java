4a5d660114504cad85e9062de0466302cd7101e0
Clarence K.C. Ho <clarenceho@gmail.com>
2023-12-07 16:05:42 -0800

    cosmetic changes

--- src/main/java/net/clarenceho/utils/Knapsack.java
+++ src/main/java/net/clarenceho/utils/Knapsack.java
@@ -3,8 +3,8 @@ package net.clarenceho.utils;
 import java.util.List;
 
 public interface Knapsack {
-
     int maximumValue(List<Knapsack.Item> items, int maxWeight);
+
     record Item(int weight, int value) {}
 }
 
